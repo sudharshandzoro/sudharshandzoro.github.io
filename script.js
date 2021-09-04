ce246c6d2db581d5f12d418a340e7ff5985e47fc
sudharshan <52266146+sudharshan-deku@users.noreply.github.com>
2021-09-04 21:45:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -142,6 +142,8 @@ var result = str.substr(0,z);
             case 35:
 	    var dor = "inurl:/database* ext:sql intext:index of -site:" + domain;
          break;
+	 case 36:
+		 var dork = "site:" + domain + " +inurl:redir+|+inurl:url+|+inurl:redirect+|+inurl:return+|+inurl:src=http+|+inurl:r=http" 
          default:
             
         }
